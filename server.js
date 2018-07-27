988e3f0785f2d3db864d038207ed13e603036290
Pasquale02 <pasquale_florio@live.it>
2018-07-27 15:40:59 +0200

    7-api-elenco-repo

--- server.js
+++ server.js
@@ -5,52 +5,57 @@ const Hapi = require('hapi');
 
 const emailSentHandler = require('./src/email-sent');
 const registeredHandler = require('./src/registered');
+const repoHandler = require('./src/yaml-repo');
 
 const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
-    routes: {
-        files: {
-            relativeTo: Path.join(__dirname, 'public')
-        }
+  port: 3000,
+  host: 'localhost',
+  routes: {
+    files: {
+      relativeTo: Path.join(__dirname, 'public')
     }
+  }
 });
 
 const init = async () => {
-    await server.register(require('inert'));
+  await server.register(require('inert'));
 
-    server.route([{
-        method: 'POST',
-        path: '/email-sent',
-        handler: emailSentHandler
-    },{
-        method: 'GET',
-        path: '/registered',
-        handler: registeredHandler
-    },{
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-            directory: {
-                path: '.',
-                redirectToSlash: true,
-                index: true,
-            }
-        }
-    }]);
+  server.route([{
+    method: 'POST',
+    path: '/email-sent',
+    handler: emailSentHandler
+  },{
+    method: 'GET',
+    path: '/registered',
+    handler: registeredHandler
+  },{
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+        index: true,
+      }
+    }
+  }, {
+    method: 'GET',
+    path: '/yaml-repo',
+    handler: repoHandler
+  }]);
 
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
 
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 
 // Crea l'indice inverso e il database delle PA
 require('./src/create-index.js')().then(() => {
-    init();
+  init();
 });
