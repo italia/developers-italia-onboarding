1a27e201d818d285dec44e8859696b144a451e3a
Pasquale02 <pasquale_florio@live.it>
2018-08-22 11:46:19 +0200

    11-configurazioni (#18)
    Implementazionedi un sistema per configurare vari parametri sia del server HTTP e sia del provider SMTP. Fix #18.

--- README.md
+++ README.md
@@ -13,16 +13,22 @@ Avviare l'applicazione con docker
 
 - Modalità sviluppo
 
+Lanciare i seguenti comandi
+
 ```bash
 docker build -t <imageName> .
-docker run -p 3000:3000 -e env=[dev|pm-dev] <imageName> 
+docker run -p 80:80 -e env=dev <imageName> 
 ```
 
 - Modalita' produzione
         
+Creare nella cartella principale del progetto un file di nome `account-config.json` basandosi sul template di `account-config-tpl.json`.
+
+Configurare il file `config-prod.json`, specificando i parametri `host` e `applicationHost`.
+
 Lanciare i seguenti comandi
 
 ```bash modalita' di produzione
 docker build -t <imageName> .
-docker run -p 3000:3000 -e env=pm-prod <imageName> 
+docker run -p 80:80 -e env=pm-prod <imageName> 
 ```
--- server.js
+++ server.js
@@ -3,11 +3,9 @@
 const Path = require('path');
 const Hapi = require('hapi');
 const Mustache = require('mustache');
+const fs = require('fs-extra');
 
-// Crea l'indice inverso e il database delle PA
-require('./src/create-index.js')().then(() => {
-  init();
-});
+const whiteList = 'account-config.json';
 
 const init = async () => {
   const emailSentHandler = require('./src/email-sent');
@@ -16,7 +14,7 @@ const init = async () => {
   const homeHandler = require('./src/home');
 
   const server = Hapi.server({
-    port: 3000,
+    port: 80,
     routes: {
       files: {
         relativeTo: Path.join(__dirname, 'public')
@@ -72,11 +70,21 @@ const init = async () => {
   }]);
 
   await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
+  console.log(`Server is running at port ${server.info.port}`);
 };
 
 process.on('unhandledRejection', (err) => {
 
   console.log(err);
   process.exit(1);
+});
+
+
+// Crea l'indice inverso e il database delle PA
+require('./src/create-index.js')().then(() => {
+  if (JSON.parse(process.argv.includes('dev')) || fs.existsSync(whiteList)) {
+    init();
+  } else {
+    console.log('Attenzione! Creare prima un file di credenziali "account-config.json" seguendo il modello di account-config-tpl.json');
+  }
 });
