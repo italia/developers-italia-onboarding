e152ba230c302a8aafb4d10fda94ad5728f1438c
Alessandro Sebastiani <sebbalex@gmail.com>
2019-04-01 13:47:42 +0200

    removed unused code due to validator implementation

--- src/email-sent.js
+++ src/email-sent.js
@@ -4,8 +4,6 @@ const fs = require('fs-extra');
 const nodemailer = require('nodemailer');
 const mustache = require('mustache');
 const jwt = require('jsonwebtoken');
-const { URL } = require('url');
-const parseDomain = require('parse-domain');
 const key = require('./get-jwt-key.js')();
 const amministrazioni = require('../public/assets/data/authorities.db.json');
 const validateUrl = require('./validator.js');
@@ -47,7 +45,7 @@ module.exports = function (request, h) {
     });
   } else {
     const configAccountString = fs.readFileSync('smtp-account-config.json').toString('utf8');
-    const accountConfig = JSON.parse(configAccountString); 
+    const accountConfig = JSON.parse(configAccountString);
 
     const transporter = nodemailer.createTransport({
       host: mailServerConfig.host,
@@ -100,74 +98,3 @@ module.exports = function (request, h) {
   let data = { pec: pec };
   return h.view('email-sent', data, { layout: 'index' });
 };
-
-/**
- * Controlla se l'URL e' ben formata e se il nome del dominio rientra in una whitelist di url.
- *
- * @param url rappresenta l'url da controllare
- *
- * @return true se la URL e' ben formata e se ientra in una whitelist, false altrimenti
- */
-function isValid(url) {
-  const generalRegex = /^(https?):\/\/(www\.)?([a-z]+)(\.([\da-zA-Z-]+)){1,2}(\/[\da-zA-Z-]+)*\/?$/;
-
-  return generalRegex.test(url) && isInWhiteList(url);
-}
-
-/**
-* Controlla se il dominio della URL passata rientra in una whitelist di domini
-*
-* @param url rappresenta l'url da controllare
-*
-* @return true se l'url e' presente nella whitelist, false altrimenti
-*/
-function isInWhiteList(url) {
-  let result = false;
-  const dictionary = {
-    'github': /^(https?):\/\/(github\.com)\/([\da-zA-Z-_])*\/?$/,
-    'bitbucket': /^(https?):\/\/(bitbucket\.org)\/([\da-zA-Z-_])*\/?$/,
-    'gitlab': /^(https?):\/\/(gitlab\.com)\/([\da-zA-Z-_]+)\/?$/,
-    'phabricator': /^(https?):\/\/(secure\.phabricator\.com)\/(p)\/([\da-zA-Z-_]+)\/?$/,
-    'gitea': /^(https?):\/\/(try\.gitea\.io)\/([\da-zA-Z-_]+)\/?$/,
-    'gogs': /^(https?):\/\/(try\.gogs\.io)\/([\da-zA-Z-_]+)\/?$/
-  };
-
-  const arrayUrl = [
-    'https://github.com/',
-    'https://bitbucket.org/',
-    'https://gitlab.com/',
-    'https://phabricator.com/',
-    'https://gitea.io/',
-    'https://gogs.io/'
-  ];
-
-  const urlParsed = new URL(url);
-  const protocol = urlParsed.protocol;
-  const hostname = urlParsed.hostname;
-
-  const domainParsed = parseDomain(url);
-  if (domainParsed != null) {
-    const domain = domainParsed.domain;
-
-    const regex = dictionary[domain];
-    const isValid = regex && regex.test(url);
-
-    let baseUrl = '';
-
-    /*  Gli url nella forma  https://try.gogs.io/<username>/<projectname>,
-                            https://try.gitea.io/<username>/<projectname>,
-                            https://secure.phabricator.com/project/view/395/
-        devono diventare    https://gogs.io/<username>/<projectname>,
-                            https://gitea.io/<username>/<projectname>,
-                            https://phabricator.com/project/view/<numProject>/
-    */
-    if (['gitea', 'gogs', 'phabricator'].includes(domain)) {
-      baseUrl = `${protocol}//${domain}.${domainParsed.tld}/`;
-    } else {
-      baseUrl = `${protocol}//${hostname}/`;
-    }
-
-    result = isValid && arrayUrl.includes(baseUrl);
-  }
-  return result;
-}
