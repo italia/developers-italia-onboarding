47ed6527cfbf7c7daabe109c08d54ceb6b08d8b1
Alessandro Sebastiani <sebbalex@gmail.com>
2019-04-02 09:28:52 +0200

    full text search enhanced, now it supports search per IPA code. #38

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,42 +1,42 @@
 function setUpSearchbar(idx, db) {
-    $('#ricercaAmministrazione').keyup(function () {
+  $('#ricercaAmministrazione').keyup(function () {
+    $('#risultatoRicerca').empty();
+
+    if (this.value.length < 3) return;
+    let query = 'description:' + this.value + ' code:' + this.value;
+    let results = idx.search(query).slice(0, 50);
+    if (results.length > 0) {
+      var resultsElem = $('#risultatoRicerca');
+
+      resultsElem.empty();
+      results.map(function (result) {
+        var obj = db[result.ref];
+        obj['code'] = result.ref;
+        return obj;
+      }).forEach(function (result) {
+        resultsElem.append(getResultElement(result));
+      });
+
+      $('.result-item').click(function () {
+        $('#ipa').val(this.dataset.code);
+        $('#nomeAmministrazione').val(this.dataset.description);
+        $('#pec').val(this.dataset.pec);
         $('#risultatoRicerca').empty();
+      });
+    }
 
-        if (this.value.length < 3) return;
-
-        var results = idx.search(this.value).slice(0, 50);
-        if (results.length > 0) {
-            var resultsElem = $('#risultatoRicerca');
-
-            resultsElem.empty();
-            results.map(function(result){
-                var obj = db[result.ref];
-                obj['code'] = result.ref;
-                return obj;
-            }).forEach(function (result) {
-                resultsElem.append(getResultElement(result));
-            });
-
-            $('.result-item').click(function() {
-                $('#ipa').val(this.dataset.code);
-                $('#nomeAmministrazione').val(this.dataset.description);
-                $('#pec').val(this.dataset.pec);
-                $('#risultatoRicerca').empty();
-            });
-        }
-
-    });
+  });
 }
 
 function getResultElement(result) {
-    return '<li class="result-item" data-code="'+result.code+'" data-pec="'+result.pec+'" data-description="'+result.description+'">' 
-         +    result.description 
-         + '</li>';
+  return '<li class="result-item" data-code="' + result.code + '" data-pec="' + result.pec + '" data-description="' + result.description + '">'
+    + result.description
+    + '</li>';
 }
 
 $.get("assets/data/authorities.index.json", function (rowIndex) {
-    $.get("assets/data/authorities.db.json", function (db) {
-        var idx = lunr.Index.load(rowIndex);
-        setUpSearchbar(idx, db);
-    });
+  $.get("assets/data/authorities.db.json", function (db) {
+    var idx = lunr.Index.load(rowIndex);
+    setUpSearchbar(idx, db);
+  });
 });
--- src/validation-error-message.js
+++ src/validation-error-message.js
@@ -1,33 +1,33 @@
-const { VALIDATION_OK, VALIDATION_NOT_WHITELIST, VALIDATION_INVALID_URL } = require('./validator-result.js');
+const {VALIDATION_OK, VALIDATION_NOT_WHITELIST, VALIDATION_INVALID_URL} = require('./validator-result.js');
 
 
 /**
-* Ottiene il messaggio di errore
-*
-* @param validatorResult Il risultato della validazione
-*
-* @return Il messaggio di errore
-*/
-function getErrorMessage(validatorResult){
-    if (validatorResult == VALIDATION_OK){
-        throw new Error('Nessun messaggio di errore definito per validator result ok');
-    }
+ * Ottiene il messaggio di errore
+ *
+ * @param validatorResult Il risultato della validazione
+ *
+ * @return Il messaggio di errore
+ */
+function getErrorMessage(validatorResult) {
+  if (validatorResult == VALIDATION_OK) {
+    throw new Error('Nessun messaggio di errore definito per validator result ok');
+  }
 
-    let message = '';
-    switch (validatorResult){
-        case VALIDATION_INVALID_URL: 
-            message = 'Indirizzo URL invalido: ricompila il form';
-            break;
-        case VALIDATION_NOT_WHITELIST: 
-            message = 'Indirizzo URL non presente nella whitelist: ricompila il form';
-            break;
-        default:
-            throw new Error('Url non valido');
-            break;
-    }
+  let message = '';
+  switch (validatorResult) {
+    case VALIDATION_INVALID_URL:
+      message = 'Indirizzo URL invalido: ricompila il form';
+      break;
+    case VALIDATION_NOT_WHITELIST:
+      message = 'Indirizzo URL non presente nella whitelist: ricompila il form';
+      break;
+    default:
+      throw new Error('Url non valido');
+      break;
+  }
 
-    return message;
+  return message;
 
 }
 
-module.exports = getErrorMessage
+module.exports = getErrorMessage;
