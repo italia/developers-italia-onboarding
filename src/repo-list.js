306c7847c493fd957d4245f9c053296a6f3dd02a
Alessandro Sebastiani <sebbalex@gmail.com>
2019-04-08 16:46:59 +0200

    repo-list has correct content-type. #54

--- src/repo-list.js
+++ src/repo-list.js
@@ -3,7 +3,8 @@
 const fs = require('fs-extra');
 const yaml = require('json2yaml');
 
-module.exports = function () {
+module.exports = function (request, h) {
+
   const whiteList = 'private/data/whitelist.db.json';
   const listRepo = fs.readFileSync(whiteList).toString('utf8');
   const jsonList = JSON.parse(listRepo);
@@ -12,8 +13,9 @@ module.exports = function () {
     jsonList.registrati.forEach((element) => delete element.referente);
     const ymlText = yaml.stringify(jsonList);
 
-    return ymlText;
+    return h.response(ymlText)
+      .header('cache-control', 'no-cache')
+      .type('text/yaml; charset=utf-8');
   }
   return 'Lista repository vuota';
-  
 };
