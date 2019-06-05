5cb1e030c60c8de3121a446edf40661d321c3eab
gtico80 <gtico80>
2019-06-05 11:51:46 +0200

    register confirm localization

--- src/register-confirm.js
+++ src/register-confirm.js
@@ -1,6 +1,7 @@
 'use strict';
 const jwt = require('jsonwebtoken');
 const key = require('./get-jwt-key.js')();
+const i18helper = require('./i18helper.js');
 
 module.exports = function (request, h) {
   const token = request.query.token;
@@ -19,5 +20,9 @@ module.exports = function (request, h) {
     pec: pec,
     amministrazione: amministrazione
   };
+  const registerConfirmCatalog = i18helper.getCatalog(request, 'register_confirm');
+  const commonCatalog = i18helper.getCatalog(request, 'common');
+  data.common = commonCatalog;
+  data.register_confirm = registerConfirmCatalog;
   return h.view('register-confirm', data, { layout: 'index' });
 };
